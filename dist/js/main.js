e6e3a21395cfe5522b1999f3c0b935a1dc6c146d
JamesIsHuang <505116512@qq.com>
2016-09-23 10:39:33 +0800

    add

--- dist/js/main.js
+++ dist/js/main.js
@@ -25,8 +25,17 @@ function homeEffect(){
     })
     //右边导航
     $('.page .active').find('.img1').attr('src','./images/yellowline.png');
-    $('.page .active').find('img').last().attr('src','./images/bigyecircle.png')
-        .css({'position':'relative','left':'-3px','top':'3px'});
+    if(/mobileplatform\.html/.test(location.href)){
+        $('.page li').css('left','0');
+        $('.page').find('img').last()
+            .css({'position':'relative','left':'100px','top':'3px'});
+        $('.page .active').find('img').last().attr('src','./images/bigyecircle.png')
+            .css({'position':'relative','left':'96px','top':'3px'});
+    }else{
+        $('.page .active').find('img').last().attr('src','./images/bigyecircle.png')
+            .css({'position':'relative','left':'-3px','top':'3px'});
+    }
+
     $('.bigdata li').mouseover(function(){
         $(this).find('img').attr('src','./images/original-hover.png');
         $(this).find('p').css('color','#ffcc66');
@@ -40,6 +49,18 @@ function homeEffect(){
     })
     $('.newscontent li').mouseout(function(){
         $(this).css('background','none').find('img').attr('src','./images/newcircle1.png');
+    })
+
+    //个人中心小人
+    $('#loginorout').click(function(){
+        if($(this).find('img').attr('src')=='./images/avatar1.png'){
+            $(this).find('img').attr('src','./images/avatar.png');
+            $('#personal').remove();
+        }else{
+            $(this).find('img').attr('src','./images/avatar1.png');
+            var html = "<div class='content'  id='personal'><img src='./images/smallsan.png'/><p><a href='./personal.html'>个人中心</a></p><p><a>账号设置</a></p><p style='border: 0'><a>退出</a></p></div>";
+            $(this).parent().after(html);
+        }
     })
 }
 
@@ -99,8 +120,14 @@ function homeEffect(){
             for (var i = 0; i < section.length; i++) {
                 var li=document.createElement('li');
                 var t=i+1;
-                li.innerHTML= '<span>'+wordDes[i]+'</span><a href=#section_'+t+' data-index=index_'+t+'>' +
-                    '<span><img class=\"img1\" src=\"./images/smallline.png\"/><img class=\"img2\" src=\"./images/smbluecircle.png\"/></span></a>';
+                if(/mobileplatform\.html/.test(location.href)){
+
+                    li.innerHTML= '<a href=#section_'+t+' data-index=index_'+t+'>' +
+                        '<span><img class=\"img2\" src=\"./images/smbluecircle.png\"/></span></a>';
+                }else{
+                    li.innerHTML= '<span id=data'+t+'>'+wordDes[i]+'</span><a href=#section_'+t+' data-index=index_'+t+'>' +
+                        '<span><img class=\"img1\" src=\"./images/smallline.png\"/><img class=\"img2\" src=\"./images/smbluecircle.png\"/></span></a>';
+                }
                 $('.page')[0].appendChild(li);
             };
             $('.page').find('li').eq(1).css({'position':'relative','left':'-12px'});
@@ -153,10 +180,20 @@ function homeEffect(){
             $('.page').find('li').removeClass('active').eq(num).addClass('active');
             //右边导航效果
             $('.img1').attr('src','./images/smallline.png');
-            $('.img2').attr('src','./images/smbluecircle.png').css({'position':'relative','left':'0','top':'0px'});
+
             $('.active .img1').attr('src','./images/yellowline.png');
-            $('.page .active').find('img').last().attr('src','./images/bigyecircle.png')
-                .css({'position':'relative','left':'-3px','top':'3px'});
+            if(/mobileplatform\.html/.test(location.href)){
+                $('.page li').css('left','0');
+                $('.img2').attr('src','./images/smbluecircle.png').css({'position':'relative','left':'100px','top':'0px'});
+                $('.page').find('img').last()
+                    .css({'position':'relative','left':'100px','top':'3px'});
+                $('.page .active').find('img').last().attr('src','./images/bigyecircle.png')
+                    .css({'position':'relative','left':'96px','top':'3px'});
+            }else{
+                $('.img2').attr('src','./images/smbluecircle.png').css({'position':'relative','left':'0','top':'0px'});
+                $('.page .active').find('img').last().attr('src','./images/bigyecircle.png')
+                    .css({'position':'relative','left':'-3px','top':'3px'});
+            }
             if (opt.orident==='vertical') {
                 if (supportsTransitions) {
                     _this.css({
